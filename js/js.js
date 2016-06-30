ae682e1d930e52aacdeebbde5e41d0d8fd305808
liuwusong <564163433@qq.com>
2016-06-30 15:16:18 +0800

    动画

--- js/js.js
+++ js/js.js
@@ -1,8 +1,7 @@
 $(function(){	
 	$(".col-sm-4").each(function(){
-		var height = $(this).find(".img").outerHeight();
+		var height = $(this).find("img").outerHeight();
 		console.log(height);
 		$(this).css("height",height);
-		$(this).find(".jianjie").css("height",height);
 	});
 });
